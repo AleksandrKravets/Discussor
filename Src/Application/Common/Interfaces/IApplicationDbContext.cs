d9f96a7f536f629a04136706b0f4701f1fb7aa56
Kravets Aleksandr <aleks260920@gmail.com>
2020-01-03 12:12:13 +0200

    Entity framework added.

---
+++ Src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        public DbSet<Theme> Themes { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Reply> PostReplies { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ Src/Infrastructure/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Contexts
+{
+    // Наследуем от интерфейса
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public DbSet<Theme> Themes { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Reply> PostReplies { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+            base.OnModelCreating(builder);
+        }
+    }
+}
