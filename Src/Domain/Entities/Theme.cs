0c4a8a9efdc57fd84d9a41be447f5c1424a0f6f9
Kravets Aleksandr <aleks260920@gmail.com>
2020-01-03 11:58:18 +0200

    Post, Reply, Theme domain models created

---
+++ Src/Domain/Entities/Theme.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Theme : StoredEntity
+    {
+        public string Title { get; set; }
+        public DateTime DateOfCreation { get; set; }
+        public byte[] Image { get; set; }
+
+        public virtual IEnumerable<Post> Posts { get; set; }
+    }
+}
