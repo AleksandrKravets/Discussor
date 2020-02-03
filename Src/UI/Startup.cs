90dbe577d6ac96e0c50fc894c2baae643d664809
Kravets Aleksandr <aleks260920@gmail.com>
2020-02-03 22:38:51 +0200

    Angular has been added.

---
+++ Src/UI/Startup.cs
@@ -0,0 +1,102 @@
+using Discussor.Core.Application;
+using Discussor.Core.Application.Common.Contracts.Services;
+using Discussor.Infrastructure;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace UI
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddApplication();
+            services.AddInfrastructure(Configuration);
+
+            services.AddControllersWithViews()
+                .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<IPostService>());
+
+            services.AddCloudscribePagination();
+
+            // In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(configuration => {
+                configuration.RootPath = "ClientApp/dist";
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            } else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            if (!env.IsDevelopment())
+            {
+                app.UseSpaStaticFiles();
+            }
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllers();
+                endpoints.MapRazorPages();
+
+                endpoints.MapAreaControllerRoute(
+                    name: null,
+                    areaName: "Admin",
+                    pattern: "Admin/{controller=Admin}/{action=Index}/{id?}");
+
+                endpoints.MapAreaControllerRoute(
+                    name: null,
+                    areaName: "Account",
+                    pattern: "Account/{controller=Account}/{action=Login}");
+
+                endpoints.MapControllerRoute(
+                    name: null,
+                    pattern: "Post/{postId}",
+                    defaults: new { controller = "Post", action = "Post" });
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
+                    name: null,
+                    pattern: "/",
+                    defaults: new { controller = "Home", action = "Index" });
+            });
+
+            app.UseSpa(spa => {
+
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
+            });
+        }
+    }
+}
