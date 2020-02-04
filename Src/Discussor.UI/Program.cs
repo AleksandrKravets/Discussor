3c9e207e54449fee894945cb3ad7943298926e68
Kravets Aleksandr <aleks260920@gmail.com>
2020-02-04 23:28:02 +0200

    Angular has been added.

---
+++ Src/Discussor.UI/Program.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discussor.Core.Domain.Entities;
+using Discussor.Infrastructure.Contracts;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Discussor.UI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Seed().Result.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+
+    public static class DatabaseSeedInitializer
+    {
+        public async static Task<IHost> Seed(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetService<IApplicationDbContext>();
+
+                try
+                {
+                    if (context.ApplicationUsers.Where(user => user.UserName == "TestAccount").FirstOrDefault() == null)
+                    {
+                        var user = new ApplicationUser
+                        {
+                            UserName = "TestAccount",
+                            Email = "testacc@mail.com",
+                            NormalizedEmail = "testacc@mail.com",
+                            EmailConfirmed = true,
+                            LockoutEnabled = false,
+                            SecurityStamp = Guid.NewGuid().ToString(),
+                            NormalizedUserName = "TestAccount",
+                        };
+
+                        context.ApplicationUsers.Add(user);
+                        var hasher = new PasswordHasher<ApplicationUser>();
+                        var hashedPassword = hasher.HashPassword(user, "TestAccount");
+                        user.PasswordHash = hashedPassword;
+
+                        await context.SaveChangesAsync();
+                    }
+                } catch (Exception)
+                {
+                }
+            }
+            return host;
+        }
+    }
+}
